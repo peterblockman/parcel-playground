3a1333dbe929ac366867fe7797be8a6d0522209a
vietvu-nguyen <vietvu.hcm@gmail.com>
2019-02-10 17:48:20 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# parcel-playground
+Parcel settings with React, Eslint, babel 7.
---
+++ index.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+
+const App = () => <div>Hello</div>
+
+ReactDom.render(<App />, document.getElementById('app'))
